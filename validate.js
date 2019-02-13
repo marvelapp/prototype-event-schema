0d676263f02889e7caeb19c838c1081897036f45
Joe Alcorn <joe.alcorn@marvelapp.com>
2019-02-13 15:32:11 +0000

    add very basic tests that the current triggers are valid

--- validate.js
+++ validate.js
@@ -1,12 +1,16 @@
 const fs = require('fs');
 const assert = require('assert');
 
-const Validator = require('jsonschema').Validator;
-const v = new Validator();
+const Ajv = new require('ajv');
+
 
 const assertValid = function(data, schema) {
-    const res = v.validate(data, schema);
-    assert(res.errors.length === 0);
+    const ajv = new Ajv({
+        format: 'full',
+        allErrors: true,
+    });
+    const valid = ajv.validate(schema, data);
+    assert(valid, `${data.type}: ${ajv.errorsText(ajv.errors)}`);
 };
 
 assertValid({
@@ -14,3 +18,35 @@ assertValid({
     coords: [0, 100],
     duration: 100,
 }, require('./triggers/tap.json'));
+
+
+
+for (var direction of ['left', 'right', 'up', 'down']) {
+    assertValid({
+        type: 'swipe',
+        direction: direction,
+        startCoords: [50, 50],
+        endCoords: [90, 55],
+        duration: 100,
+    }, require('./triggers/swipe.json'))
+}
+
+for (var direction of ['in', 'out']) {
+    assertValid({
+        type: 'pinch',
+        direction: direction,
+        coords: [50, 50],
+        duration: 100,
+    }, require('./triggers/pinch.json'))
+}
+
+
+assertValid({
+    type: 'timer',
+}, require('./triggers/timer.json'))
+
+
+// assertValid({
+//     type: 'hover',
+//     duration: 1250,
+// }, require('./triggers/hover.json'))

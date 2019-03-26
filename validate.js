8151ec799ce880692b029e1fea83ab8fde6bf120
Mark Allison <mark@marvelapp.com>
2019-03-26 10:55:15 +0000

    Finesse overlays

--- validate.js
+++ validate.js
@@ -3,58 +3,75 @@ const flake = new FlakeId();
 
 const assertValid = require('./assertValid').assertValid;
 
-assertValid({
-    trigger: {type: "tap"},
-    object: {type: "hotspot"},
-    outcome: {type: "screenTransition"},
-    timestamp: 650,
-    prevId: flake.gen(),
-    id: flake.gen(),
-}, require('./event.json'))
+assertValid(
+    {
+        trigger: { type: 'tap' },
+        object: { type: 'hotspot' },
+        outcome: { type: 'screenTransition' },
+        timestamp: 650,
+        prevId: flake.gen(),
+        id: flake.gen(),
+    },
+    require('./event.json')
+);
 
-assertValid({
-    type: 'tap',
-    coords: [0, 100],
-    duration: 100,
-}, require('./triggers/tap.json'));
+assertValid(
+    {
+        type: 'tap',
+        coords: [0, 100],
+        duration: 100,
+    },
+    require('./triggers/tap.json')
+);
 
-assertValid({
-    type: 'doubletap',
-    coords: [0, 100],
-    duration: 100,
-}, require('./triggers/doubletap.json'));
-
+assertValid(
+    {
+        type: 'doubletap',
+        coords: [0, 100],
+        duration: 100,
+    },
+    require('./triggers/doubletap.json')
+);
 
 for (var direction of ['left', 'right', 'up', 'down']) {
-    assertValid({
-        type: 'swipe',
-        direction: direction,
-        startCoords: [50, 50],
-        endCoords: [90, 55],
-        duration: 100,
-    }, require('./triggers/swipe.json'))
+    assertValid(
+        {
+            type: 'swipe',
+            direction: direction,
+            startCoords: [50, 50],
+            endCoords: [90, 55],
+            duration: 100,
+        },
+        require('./triggers/swipe.json')
+    );
 }
 
 for (var direction of ['in', 'out']) {
-    assertValid({
-        type: 'pinch',
-        direction: direction,
-        coords: [50, 50],
-        duration: 100,
-    }, require('./triggers/pinch.json'))
+    assertValid(
+        {
+            type: 'pinch',
+            direction: direction,
+            coords: [50, 50],
+            duration: 100,
+        },
+        require('./triggers/pinch.json')
+    );
 }
 
+assertValid(
+    {
+        type: 'timer',
+    },
+    require('./triggers/timer.json')
+);
 
-assertValid({
-    type: 'timer',
-}, require('./triggers/timer.json'))
-
-
-assertValid({
-    type: 'hover',
-    duration: 1250,
-}, require('./triggers/hover.json'))
-
+assertValid(
+    {
+        type: 'hover',
+        duration: 1250,
+    },
+    require('./triggers/hover.json')
+);
 
 const transitionAnimations = [
     null,
@@ -69,33 +86,43 @@ const transitionAnimations = [
     'flip',
     'flow',
     'slide-fade',
-]
+];
 for (var animation of transitionAnimations) {
-    assertValid({
-        type: 'screenTransition',
-        fromScreen: 1,
-        toScreen: 2,
-        animation: animation
-    }, require('./outcomes/screenTransition.json'))
+    assertValid(
+        {
+            type: 'screenTransition',
+            fromScreen: 1,
+            toScreen: 2,
+            animation: animation,
+        },
+        require('./outcomes/screenTransition.json')
+    );
 }
 
-assertValid({
-    type: 'overlay',
-    screen: 2,
-    position: [50, 50],
-}, require('./outcomes/overlay.json'))
+assertValid(
+    {
+        type: 'overlayTransition',
+        screen: 2,
+        position: [50, 50],
+    },
+    require('./outcomes/overlayTransition.json')
+);
 
+assertValid(
+    {
+        type: 'openUrl',
+        url: 'https://blog.marvelapp.com',
+        newWindow: true,
+    },
+    require('./outcomes/openUrl.json')
+);
 
-assertValid({
-    type: 'openUrl',
-    url: 'https://blog.marvelapp.com',
-    newWindow: true,
-}, require('./outcomes/openUrl.json'))
-
-
-assertValid({
-    type: 'screen',
-    id: 1,
-    height: 100,
-    width: 100,
-}, require('./objects/screen.json'))
+assertValid(
+    {
+        type: 'screen',
+        id: 1,
+        height: 100,
+        width: 100,
+    },
+    require('./objects/screen.json')
+);
